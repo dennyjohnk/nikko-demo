a031e5cffee4b3f00e3381466d67a599c4a03e59
dennyjohnk <denny.john@asista.com>
2021-02-13 12:36:07 +0530

    add build

---
+++ build/precache-manifest.e585c72ea3638223407432c710954383.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179648a4ad535945f833286920ba9349",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c8d903455ee189960b4",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "93fc0edd711fd6842ee8",
+    "url": "./static/css/main.7e242f92.chunk.css"
+  },
+  {
+    "revision": "0c8d903455ee189960b4",
+    "url": "./static/js/2.090b3c6c.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "./static/js/2.090b3c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93fc0edd711fd6842ee8",
+    "url": "./static/js/main.636b136f.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
