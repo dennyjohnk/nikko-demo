47dd88d93bbb0aebffc1ca7a2cc9414bb8f9af46
dennyjohnk <denny.john@asista.com>
2021-02-13 12:27:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
-import Experience from "./components/Experience";
-import Projects from "./components/Projects";
+//import Experience from "./components/Experience";
+//import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 class App extends Component {
@@ -121,18 +121,18 @@ class App extends Component {
           resumeBasicInfo={this.state.resumeData.basic_info}
           sharedBasicInfo={this.state.sharedData.basic_info}
         />
-        <Projects
+        {/* <Projects
           resumeProjects={this.state.resumeData.projects}
           resumeBasicInfo={this.state.resumeData.basic_info}
-        />
+        /> */}
         <Skills
           sharedSkills={this.state.sharedData.skills}
           resumeBasicInfo={this.state.resumeData.basic_info}
         />
-        <Experience
+        {/* <Experience
           resumeExperience={this.state.resumeData.experience}
           resumeBasicInfo={this.state.resumeData.basic_info}
-        />
+        /> */}
         <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
       </div>
     );
